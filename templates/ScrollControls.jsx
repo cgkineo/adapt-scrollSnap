826ae5273ead1159a66c71684735988b32f40f74
Oliver Foster <oliver.foster@kineo.com>
2022-05-31 10:13:42 +0100

    Menu/notify button aria-labels fix

--- templates/ScrollControls.jsx
+++ templates/ScrollControls.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 export default function NotifyScrollControls(props) {
-  const { _buttons, _prompts } = props;
-  const {
-    _previous,
-  } = _buttons;
+  const { _prompts } = props;
   const {
     _scroll
   } = _prompts;
@@ -15,7 +12,7 @@ export default function NotifyScrollControls(props) {
         className='btn-icon scrollsnap__controls-btn scrollsnap__controls-btn-up'
         onClick={props.onClick}
         data-direction='up'
-        aria-label={_previous.label}
+        aria-label={_scroll.previousLabel}
       >
         <div className='icon icon-controls-up' aria-hidden='true'></div>
       </button>
@@ -23,7 +20,7 @@ export default function NotifyScrollControls(props) {
         className='btn-icon scrollsnap__controls-btn scrollsnap__controls-btn-down'
         onClick={props.onClick}
         data-direction='down'
-        aria-label={_scroll.label}
+        aria-label={_scroll.nextLabel}
       >
         <div className='icon icon-controls-down' aria-hidden='true'></div>
       </button>
